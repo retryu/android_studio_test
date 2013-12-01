1c5d22a012028ed3ec34bcd9cab357423979a1e8
retryu <ruanchenyugood@gmail.com>
2013-12-01 14:34:46 +0800

    Initial commit

---
+++ java/com/example/myapplication222/MyModel.java
@@ -0,0 +1,24 @@
+package com.example.myapplication222;
+
+/**
+ * Created by retryu on 13-12-1.
+ */
+public class MyModel {
+    private String name;
+    private String age;
+
+
+
+    public MyModel(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
